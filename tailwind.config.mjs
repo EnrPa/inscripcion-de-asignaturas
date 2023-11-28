dd9c5bc6c8e250a0437eb0e47de16e63b044321b
substicker <yttrium@tuta.io>
2023-11-27 21:39:16 -0300

    añadido colores de la universidad

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary1: '#FEDA3F',
+				primary2: '#3C3C3B',
+				secondary1: '#A4222B',
+				secondary2: '#12636B',
+				text: '#444242',
+				background: '#444242',
+			},
+			backgroundImage:{
+				'crowd-walking': "url('/public/crowd-walking.gif')",
+			}
+		},
 	},
 	plugins: [],
 }

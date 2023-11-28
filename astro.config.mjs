3fc7024c39226f74f5c7c5b1e7914d798a6815d9
substicker <yttrium@tuta.io>
2023-11-27 21:37:51 -0300

    añadido paginas estáticas .astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+import solidJs from "@astrojs/solid-js";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), solidJs(), tailwind()]
+});
